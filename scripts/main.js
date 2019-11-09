bf078621781dc655d6daa9c8ad2e714c83a89abc
Kelsey Williams <ckelseytheewilliams@gmail.com>
2019-11-08 18:46:51 -0800

    bug fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 var el = document.querySelectorAll('.hamburger');
-for(i=0; i<=el.length; i++) {
+for(i=0; i<=el.length-1; i++) {
   el[i].addEventListener('click', function() {
     this.classList.toggle('active');
   }, false);
